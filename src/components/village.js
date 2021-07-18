58211bbd46f87dc8b532484427922c54b430c8cf
Tomas Morton <tomas.morton@hotmail.co.nz>
2021-07-18 14:14:51 -0400

    Add Audio

--- src/components/village.js
+++ src/components/village.js
@@ -6,6 +6,7 @@ Crafty.c('Village', {
 
     collect: function() {
         this.destroy();
+        Crafty.audio.play('knock');
         Crafty.trigger('VillageVisited', this);
     }
 });
--- src/scenes/loading.js
+++ src/scenes/loading.js
@@ -1,3 +1,11 @@
+const assets = [
+    'assets/16x16_forest_1.gif',
+    'assets/hunter.png',
+    'door_knock_3x.mp3',
+    'door_knock_3x.ogg',
+    'door_knock_3x.aac'
+];
+
 Crafty.scene('Loading',
     function() {
         const displayLoadingMessage = function() {
@@ -7,19 +15,30 @@ Crafty.scene('Loading',
                 .css($text_css);
         };
 
-        displayLoadingMessage();
-        // Load our sprite map image
-        Crafty.load(['assets/16x16_forest_1.gif', 'assets/hunter.png'],
-            function() {
-                Crafty.sprite(16, 'assets/16x16_forest_1.gif', {
-                    spr_tree: [0, 0],
-                    spr_bush: [1, 0],
-                    spr_village: [0, 1],
-                });
-                Crafty.sprite(16, 'assets/hunter.png', {
-                    spr_player: [0, 2],
-                }, 0, 2);
+        const addSprites = function() {
+            Crafty.sprite(16, 'assets/16x16_forest_1.gif', {
+                spr_tree: [0, 0],
+                spr_bush: [1, 0],
+                spr_village: [0, 1],
+            });
+            Crafty.sprite(16, 'assets/hunter.png', {
+                spr_player: [0, 2],
+            }, 0, 2);
+        };
 
+        const addAudio = function() {
+            Crafty.audio.add({
+                knock: ['assets/door_knock_3x.mp3',
+                    'assets/door_knock_3x.ogg',
+                    'assets/door_knock_3x.aac']
+            });
+        };
+
+        displayLoadingMessage();
+        Crafty.load(assets,
+            function() {
+                addSprites();
+                addAudio();
                 Crafty.scene('Game');
             });
     });
